05d2e203b89105f4e3536c1e95bcfc9d59664298
kitzapat-svg <kitzapat@gmail.com>
2026-02-02 14:57:00 +0700

    Create proxy.ts

---
+++ proxy.ts
@@ -0,0 +1,13 @@
+import { withAuth } from "next-auth/middleware"
+
+// เปลี่ยนจากการ export default มาเป็น export const proxy
+export const proxy = withAuth({
+  pages: {
+    signIn: '/api/auth/signin', // ให้เด้งไปหน้าล็อกอินอัตโนมัติ
+  },
+})
+
+// Config ยังใช้เหมือนเดิมครับ
+export const config = { 
+  matcher: ["/staff/:path*"] 
+}
